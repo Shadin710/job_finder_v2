aa9df9872780a321ce728aebff671d7c28c6fef2
Shadin710 <shadin710@gmail.com>
2026-02-26 00:10:10 +1030

    rolled back

--- connection/db_connection.php
+++ connection/db_connection.php
@@ -2,15 +2,11 @@
 
 
 <?php
+require_once __DIR__ . '/../config.php';
 
-$host = getenv('DB_HOST');
-$user = getenv('DB_USER');
-$pass = getenv('DB_PASS');
-$name = getenv('DB_NAME');
+$conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
-$conn = mysqli_connect($host, $user, $pass, $name);
-
-if (!$conn) {
+if(!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 ?>
